9b51ee6aabf5aa7dd99d9b1108484c66467527ee
mammadli <gulnar.mammadli@ut.ee>
2023-02-09 01:47:41 +0200

    iInitial commit

---
+++ src/main/java/com/mammadli/user_crud/services/impl/UserServicesImpl.java
@@ -0,0 +1,47 @@
+package com.mammadli.user_crud.services.impl;
+
+import com.mammadli.user_crud.db.dto.UserDto;
+import com.mammadli.user_crud.db.entity.User;
+import com.mammadli.user_crud.db.repository.UserRepository;
+import com.mammadli.user_crud.mapper.UserMapper;
+import com.mammadli.user_crud.services.UserServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserServicesImpl implements UserServices {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public User create(UserDto userDto) {
+        User user = userMapper.mapToUser(userDto);
+        user.setUserName(user.getName().concat(".").concat(user.getSurname()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User update(String userId) {
+        return null;
+    }
+
+    @Override
+    public User getUser(String userId) {
+        Optional<User> user = userRepository.findById(userId);
+        return user.orElse(null);
+    }
+
+    @Override
+    public Void deleteUser(String userId) {
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isPresent()){
+            userRepository.deleteById(user.get().getId());
+            userRepository.save(user.get());
+        }
+        return null;
+    }
+}
