491742fd232cdb4b5a882740e0d5b3303d997707
mammadli <gulnar.mammadli@ut.ee>
2023-02-09 01:45:22 +0200

    Add @ComponentScan

--- src/main/java/com/mammadli/user_crud/UserCrudApplication.java
+++ src/main/java/com/mammadli/user_crud/UserCrudApplication.java
@@ -2,8 +2,10 @@ package com.mammadli.user_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.mammadli.user_crud.mapper"})
 public class UserCrudApplication {
 
     public static void main(String[] args) {
