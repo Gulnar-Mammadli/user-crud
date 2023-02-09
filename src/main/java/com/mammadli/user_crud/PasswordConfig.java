1c1fdaf520ade72dc6008d885044b6fe3f86dcd1
mammadli <gulnar.mammadli@ut.ee>
2023-02-09 20:08:47 +0200

    Updated

--- src/main/java/com/mammadli/user_crud/PasswordConfig.java
+++ src/main/java/com/mammadli/user_crud/PasswordConfig.java
@@ -11,7 +11,7 @@ public class PasswordConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+
         return new BCryptPasswordEncoder();
     }
-
 }
--- src/main/java/com/mammadli/user_crud/UserCrudApplication.java
+++ src/main/java/com/mammadli/user_crud/UserCrudApplication.java
@@ -2,10 +2,8 @@ package com.mammadli.user_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.mammadli.user_crud.mapper"})
 public class UserCrudApplication {
 
     public static void main(String[] args) {
