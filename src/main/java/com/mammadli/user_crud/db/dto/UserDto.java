b747b122693a2fa43039b2754c745923f04ec342
mammadli <gulnar.mammadli@ut.ee>
2023-02-09 05:26:32 +0200

    Remove userName field

--- src/main/java/com/mammadli/user_crud/db/dto/UserDto.java
+++ src/main/java/com/mammadli/user_crud/db/dto/UserDto.java
@@ -1,6 +1,5 @@
 package com.mammadli.user_crud.db.dto;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,9 +15,5 @@ public class UserDto {
     private String surname;
     private String email;
     private String phoneNumber;
-
-    @Column(unique = true)
-    private String userName;
-
     private String password;
 }
