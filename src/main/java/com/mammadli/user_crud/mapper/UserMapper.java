3582faea11dec28a3d51867dae015252ef2aa609
mammadli <gulnar.mammadli@ut.ee>
2023-02-09 20:07:24 +0200

    Add componentModel

--- src/main/java/com/mammadli/user_crud/mapper/UserMapper.java
+++ src/main/java/com/mammadli/user_crud/mapper/UserMapper.java
@@ -2,20 +2,14 @@ package com.mammadli.user_crud.mapper;
 
 import com.mammadli.user_crud.db.dto.UserDto;
 import com.mammadli.user_crud.db.dto.UserGetDto;
-import com.mammadli.user_crud.db.dto.UserUpdateDto;
 import com.mammadli.user_crud.db.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.NullValueMappingStrategy;
 
 
-@Mapper(nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 
     User mapToUser(UserDto userDto);
-
-    User fromUserUpdateDto(UserUpdateDto userUpdateDto);
-
-    UserUpdateDto mapToUserUpdateDto(User user);
 
     UserGetDto mapToUserGetDto(User user);
 
