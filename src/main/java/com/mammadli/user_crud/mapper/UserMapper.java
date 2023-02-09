ff803b5f93f668fd4836e0ab825a5dc1f19b44d2
mammadli <gulnar.mammadli@ut.ee>
2023-02-09 02:38:27 +0200

    Add new mappers

--- src/main/java/com/mammadli/user_crud/mapper/UserMapper.java
+++ src/main/java/com/mammadli/user_crud/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package com.mammadli.user_crud.mapper;
 
 import com.mammadli.user_crud.db.dto.UserDto;
+import com.mammadli.user_crud.db.dto.UserGetDto;
+import com.mammadli.user_crud.db.dto.UserUpdateDto;
 import com.mammadli.user_crud.db.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValueMappingStrategy;
@@ -11,5 +13,10 @@ public interface UserMapper {
 
     User mapToUser(UserDto userDto);
 
-    UserDto mapToUserDto(User user);
+    User fromUserUpdateDto(UserUpdateDto userUpdateDto);
+
+    UserUpdateDto fromUser(User user);
+
+    UserGetDto mapToUserGetDto(User user);
+
 }
